7d14924a009dcf4884ce39fbc88fc71c971a3826
Lairton <130435095+Laiirton@users.noreply.github.com>
2026-03-20 01:12:38 -0300

    Update database.go

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,10 +2,17 @@ package database
 
 import (
 	"database/sql"
+	"embed"
 	"fmt"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	_ "github.com/lib/pq"
 )
+
+//go:embed migrations/*.sql
+var migrationsFS embed.FS
 
 func Connect(dbURL string) (*sql.DB, error) {
 	db, err := sql.Open("postgres", dbURL)
@@ -21,4 +28,32 @@ func Connect(dbURL string) (*sql.DB, error) {
 	db.SetMaxIdleConns(5)
 
 	return db, nil
+}
+
+func RunMigrations(db *sql.DB) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("could not create database driver: %w", err)
+	}
+
+	source, err := iofs.New(migrationsFS, "migrations")
+	if err != nil {
+		return fmt.Errorf("could not create source driver: %w", err)
+	}
+
+	m, err := migrate.NewWithInstance(
+		"iofs",
+		source,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return fmt.Errorf("could not create migrate instance: %w", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("could not run up migrations: %w", err)
+	}
+
+	return nil
 }
